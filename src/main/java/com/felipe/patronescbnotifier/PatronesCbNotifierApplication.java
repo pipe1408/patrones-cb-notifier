463b1d65f1dac05a63dc84c543e75140de8d04a8
Felipe <pipeba1408@gmail.com>
2024-09-05 13:36:24 -0500

    Initial commit

---
+++ src/main/java/com/felipe/patronescbnotifier/PatronesCbNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.patronescbnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronesCbNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatronesCbNotifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/felipe/patronescbnotifier/PatronesCbNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.patronescbnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronesCbNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
